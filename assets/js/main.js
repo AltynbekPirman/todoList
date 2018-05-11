f6701a6b30eb72396141f4f8b641c41ed37080fd
Altynbek Pirman <altynbek.pirman@nu.edu.kz>
2018-05-11 19:58:36 +0600

    added delete feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $("input[type='text']").on("keypress", function(e) {
   if(e.which === 13){
-    var todoText = $(this).val() + "?";
+    var todoText = $(this).val() + "%#";
     if(localStorage.getItem('todos') === null) {
         var todoItems = []
         todoItems.push(todoText)
@@ -12,17 +12,33 @@ $("input[type='text']").on("keypress", function(e) {
         todoItems.push(todoText)
         localStorage.setItem('todos', todoItems);
         $(this).val("")
-        $("ul").append("<li>"+"<span><i class='fa fa-trash'></i></span> "+todoText.slice(0,-1)+"</li>")
+        $("ul").append("<li>"+"<span><i class='fa fa-trash'></i></span>"+todoText.slice(0,-2)+"</li>")
     }
   }
 })
 
 function displayItems() {
     if(localStorage.getItem('todos') != null){
-        var todoItemsString = localStorage.getItem('todos').slice(0,-1);
-        var todoItems = todoItemsString.split('?,');
+        var todoItemsString = localStorage.getItem('todos').slice(0,-2);
+        var todoItems = todoItemsString.split('%#,');
         for(i=0;i < todoItems.length;i++) {
-            $("ul").append("<li>"+"<span><i class='fa fa-trash'></i></span> "+todoItems[i]+"</li>")
+            $("ul").append("<li>"+"<span><i class='fa fa-trash'></i></span>"+todoItems[i]+"</li>")
         }
     }
 }
+
+$('ul').on('click', 'span', function() {
+    todoItems = (localStorage.getItem('todos')+',');
+    todoItems = todoItems.split("%#,");
+    todoItem = this.parentElement.textContent;
+    ind = todoItems.indexOf(todoItem);
+    if(ind > -1) {
+        todoItems.splice(ind, 1);
+        if(todoItems.length == 1 && todoItems[0] == "") {
+            localStorage.clear()
+        } else {
+            localStorage.setItem('todos', todoItems.join("%#,").slice(0,-1));
+        }
+        location.reload();
+    }
+})
